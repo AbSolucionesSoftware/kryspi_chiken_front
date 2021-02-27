c70779041754de91a367e29120c7ffd75af62b14
Diego <consultoresartificialbrain@gmail.com>
2021-02-27 11:31:28 -0600

    agregando todo

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c611ab586b07e3584d0f412e135c50f1.js"
+  "/precache-manifest.e88e171c75636e151708a4b18c1f8922.js"
 );
 
 self.addEventListener('message', (event) => {
